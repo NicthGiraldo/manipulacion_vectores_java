e1a8f06a07341ab8476391b2dd6a530fb9229420
Nicth Giraldo <nicth.giraldo@gmail.com>
2026-08-19 22:00:48 -0500

    primer commit de prueba para verficar vinculacion con repositorio

--- manipulacion_vectores.java
+++ manipulacion_vectores.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
+class reception {
+    protected static Scanner sc = new Scanner(System.in);
+}
+
 public class manipulacion_vectores {
-    
+    public static void main(String[] args) {
+
+    }
 }
